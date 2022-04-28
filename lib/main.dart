48450fe1bbfb4f6915ed9e4cc215e3679020d412
ASUS <eb.mehrdad@gmail.com>
2022-04-28 13:33:16 +0430

    transAction dart file created

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,59 @@
-
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+import 'transaction.dart';
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  final List <Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center ,
+
+        children: <Widget>[
+          Container(
+            width: double.infinity, // means full width of the screen (100%)
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('LIST OF TX'),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/transaction.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  String id;
+  String title;
+  double amount;
+  DateTime date;
+
+  Transaction({ 
+    @required this.id, // @required means that the id is required
+     @required this.title,
+      @required this.amount,
+      @required this.date
+      });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'date': date.toIso8601String()
+    };
+  }
+
+  static Transaction fromMap(Map<String, dynamic> map) {
+    return Transaction(
+      id: map['id'],
+      title: map['title'],
+      amount: map['amount'],
+      date: DateTime.parse(map['date'])
+    );
+  }
+}
